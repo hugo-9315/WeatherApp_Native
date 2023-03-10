14acbcd31ea8b5f1bce4d5d703bcdb795d13cb01
Hugo <hugo.marchao@ynov.com>
2023-03-10 14:25:26 +0100

    first commit

---
+++ components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Index = () => {
+  return <Header />;
+
+  const Header = styled.View`
+    height: 150px;
+    background-color: lightblue;
+  `;
+};
+
+export default Index;
